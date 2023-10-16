f0dede92fd82298293984272a39643d73fefacdb
Jahangeer.i <jahangeer7704@gmail.com>
2023-10-16 20:37:50 +0530

    major update on vedio play

--- app.js
+++ app.js
@@ -1,180 +1,196 @@
 
-    let mainDoc = document.querySelector("main")
-    let video = document.querySelector("video")
-
-    video.setAttribute("autoplay",null)
-
-
-    mainDoc.addEventListener("click", (e) => {
-/*         let target = e.target.innerText.length === 1 ? e.target.innerText : e.target.previousSibling.innerText
- */ 
-
-let target=e.target.innerText
-let  targetELementClass=()=>{
-
-    if(e.target.tagName==="SPAN"  && target.length===1){
-        let targetVideo=e.target.previousElementSibling.classList.value
-        return targetVideo;
-        
-    }
-    else if(e.target.tagName==="SPAN"  && target.length>1){
-        let targetVideo=e.target.previousElementSibling.previousElementSibling.classList.value
-        return targetVideo;
-        
-    
-    }
-    else{
-        return null
-    }
-}
-ChoosenClassSender=targetELementClass()
-
-vedioChooser(ChoosenClassSender)
+let mainDoc = document.querySelector("main")
+let body = document.querySelector("body > div")
+let video = document.createElement("video")
+let div = document.createElement("div")
+let title = document.createElement("span")
+title.className = "title-span"
+let text = ""
+div.className = "video-container"
+body.append(div)
 
 
 
-    })
-    window.addEventListener("keyup", (e) => {
-
-switch (e.key.toUpperCase()) {
-    case "P":
-          vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-    case "Q":
-          vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
+video.setAttribute("autoplay", null)
 
 
-    case "R":
-          vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
+mainDoc.addEventListener("click", (e) => {
+    let target = e.target.innerText.length === 1 ? e.target.innerText : e.target.previousSibling.innerText
+    vedioChooser(target)
 
 
-    case "S":
-        vedioChooser(`videos-${e.key.toUpperCase()}`)
-
-     
-        break;
-
-
-    case "1":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "2":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "3":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "4":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "5":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "6":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "7":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "8":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "9":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "A":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "B":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "C":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "D":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "E":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "F":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "G":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-
-    case "H":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "M":
-         vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "J":
-        vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "K":
-        vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-
-    case "L":
-        vedioChooser(`videos-${e.key.toUpperCase()}`)
-        break;
-
-    default:
-        null
-
-}
-    })
-   function vedioChooser(target)  {
-
-let caughtClass=document.querySelector(`.${target}`)
-caughtClass.style.display="flex";
-
-caughtClass.addEventListener("ended",()=>{
-    caughtClass.style.display="none"
 })
+window.addEventListener("keyup", (e) => {
+    vedioChooser(e.key)
+})
+function videoSetter(userInput, text) {
+    div.style.display = "flex"
+    title.textContent = text
+    div.append(title)
+    div.append(video)
+    video.setAttribute("src", `${userInput}.mp4`)
+
+}
+function vedioChooser(target) {
+    let userInput = target.toUpperCase()
+    switch (userInput) {
+        case "P":
+            text = "Principal Room"
+            videoSetter(userInput, text);
+            break;
+
+        case "Q":
+            text = "CSE HOD Room"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "R":
+            text = "CSE Staff Room"
+            videoSetter(userInput, text)
+            break;
+
+
+
+
+
+        case "1":
+            text = "CS Lab 1"
+            videoSetter(userInput, text);
+            break;
+
+
+        case "2":
+            text = "CS Lab 2"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "3":
+            text = "CS Lab 3"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "4":
+            text = "CS Lab 4"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "5":
+            text = "Physics Lab"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "6":
+            text = "Chemistry Lab"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "7":
+            text = "Electrical Lab"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "8":
+
+            text = "CSE 2nd Year A-Sec"
+            videoSetter(userInput, text)
+            break;
+
+        case "9":
+            text = "CSE 2nd Year B-Sec"
+
+            videoSetter(userInput, text)
+            break;
+
+
+        case "A":
+            text = "CSE 3rd Year A-Sec"
+
+            videoSetter(userInput, text)
+            break;
+
+
+        case "B":
+            text = "CSE 3rd Year B-Sec"
+
+            videoSetter(userInput, text)
+            break;
+
+
+        case "C":
+            text = "CSE 4th Year"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "D":
+            text = "AI&DS 2nd Year B-sec"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "E":
+            text = "Gents Rest Room 1"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "F":
+            text = "Gents Rest Room 2"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "G":
+            text = "Ladies Rest Room 1"
+            videoSetter(userInput, text)
+            break;
+        case "H":
+            text = "Ladies Rest Room 2"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "M":
+            text = "Seminar Hall"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "J":
+            text = "Smart Room"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "K":
+            text = "Server Room"
+            videoSetter(userInput, text)
+            break;
+
+
+        case "L":
+            text = "AI&DS Staff Room"
+            videoSetter(userInput, text)
+            break;
+
+        default:
+            null
+
     }
 
+}
 
 
-
+video.addEventListener("ended", () => {
+    let duration = setInterval(() => {
+        div.style.display = "none"
+        clearInterval(duration)
+    }, 2000)
+})
