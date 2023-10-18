785a345771b7d63b055eaaabec7e353b4f9854ff
Jahangeer.i <jahangeer7704@gmail.com>
2023-10-18 21:34:21 +0530

    buttons-behaviour only on key inputs

--- app.js
+++ app.js
@@ -5,45 +5,44 @@ let div = document.createElement("div");
 let title = document.createElement("span");
 let spinner = document.createElement("div");
 let text = "";
+let keyLog = [];
 
 /* buttons */
 let button = document.createElement("div");
 
 let playPauseParent = document.createElement("div");
-let playPauseChildSymbol = document.createElement("div")
-playPauseChildSymbol.className = ("pauseSymbol");
-playPauseChildSymbol.textContent="P"
-let playPauseChildText = document.createElement("div")
-playPauseChildText.className = ("pauseText");
-playPauseChildText.textContent="PAUSE"
-playPauseParent.append(playPauseChildSymbol, playPauseChildText)
+let playPauseChildSymbol = document.createElement("div");
+playPauseChildSymbol.className = "pauseSymbol";
+playPauseChildSymbol.textContent = "P";
+let playPauseChildText = document.createElement("div");
+playPauseChildText.className = "pauseText";
+playPauseChildText.textContent = "PAUSE";
+playPauseParent.append(playPauseChildSymbol, playPauseChildText);
 
 let quitParent = document.createElement("div");
-let quitChildSymbol = document.createElement("div")
-quitChildSymbol.className = ("quitSymbol");
-quitChildSymbol.textContent="Q"
-let quitChildText = document.createElement("div")
-quitChildText.className = ("quitText");
-quitChildText.textContent="QUIT"
-quitParent.append(quitChildSymbol, quitChildText)
+let quitChildSymbol = document.createElement("div");
+quitChildSymbol.className = "quitSymbol";
+quitChildSymbol.textContent = "Q";
+let quitChildText = document.createElement("div");
+quitChildText.className = "quitText";
+quitChildText.textContent = "QUIT";
+quitParent.append(quitChildSymbol, quitChildText);
 
 let replayParent = document.createElement("div");
-let replayChildText = document.createElement("div")
-replayChildText.className = ("replayText");
-replayChildText.textContent="REPLAY"
-let replayChildSymbol = document.createElement("div")
-replayChildSymbol.className = ("replaySymbol");
-replayChildSymbol.textContent="R"
+let replayChildText = document.createElement("div");
+replayChildText.className = "replayText";
+replayChildText.textContent = "REPLAY";
+let replayChildSymbol = document.createElement("div");
+replayChildSymbol.className = "replaySymbol";
+replayChildSymbol.textContent = "R";
 
-replayParent.append(replayChildSymbol, replayChildText)
+replayParent.append(replayChildSymbol, replayChildText);
 
-
-button.className = "button-parent"
-playPauseParent.className = "play-pause"
-quitParent.className = "quitParent"
-replayParent.className = "replayParent"
-button.append(quitParent, playPauseParent, replayParent)
-
+button.className = "button-parent";
+playPauseParent.className = "play-pause";
+quitParent.className = "quitParent";
+replayParent.className = "replayParent";
+button.append(quitParent, playPauseParent, replayParent);
 
 //ends here
 spinner.className = "spinner";
@@ -52,48 +51,62 @@ div.className = "video-container";
 body.append(div);
 
 video.setAttribute("autoplay", null);
-
+video.controlsList = "nodownload";
 mainDoc.addEventListener("click", (e) => {
   let target =
     e.target.innerText.length === 1
       ? e.target.innerText
       : e.target.previousSibling.innerText;
-  vedioChooser(target);
+  if (target) {
+    keyLog.push(target.toUpperCase());
+    videoChooser(target);
+  }
+});
+window.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    e.preventDefault();
+  }
 });
 window.addEventListener("keyup", (e) => {
-  vedioChooser(e.key);
+  videoChooser(e.key);
+  keyLog.push(e.key.toUpperCase());
+  let inputForVideoController =
+    e.key === " " ? " " : e.key === "q" ? "Q" : e.key === "r" ? "R" : null;
+  if (inputForVideoController) {
+    buttonInputHandler(inputForVideoController, keyLog);
+  }
 });
 function videoSetter(userInput, text) {
   div.style.display = "flex";
   title.textContent = text;
   div.append(title);
   div.append(spinner);
-button.remove()
+  button.remove();
 
   video.style.display = "none";
 
   video.setAttribute("src", `${userInput}.mp4`);
   video.addEventListener("canplaythrough", () => {
-    spinner.remove()
+    spinner.remove();
     video.style.display = "block";
     div.append(video);
-    div.append(button)
+    div.append(button);
   });
 }
-function vedioChooser(target) {
+function videoChooser(target) {
   let userInput = target.toUpperCase();
   switch (userInput) {
-    case "P":
+    case "X":
       text = "Principal Room";
       videoSetter(userInput, text);
       break;
 
-    case "Q":
+    case "Y":
       text = "CSE HOD Room";
       videoSetter(userInput, text);
       break;
 
-    case "R":
+    case "Z":
       text = "CSE Staff Room";
       videoSetter(userInput, text);
       break;
@@ -216,6 +229,23 @@ function vedioChooser(target) {
   }
 }
 
+function buttonInputHandler(input, keyLog) {
+  input === "Q"
+    ? videoQuit()
+    : input === "R"
+    ? videoReplay(keyLog)
+    : playPause();
+}
+function videoQuit() {
+  div.style.display = "none";
+}
+function videoReplay(keyLog) {
+  keyLog.pop();
+  let index = keyLog.length - 1;
+  videoChooser(keyLog[index]);
+}
+function playPause() {}
+
 video.addEventListener("ended", () => {
   let duration = setInterval(() => {
     div.style.display = "none";
@@ -223,4 +253,3 @@ video.addEventListener("ended", () => {
     clearInterval(duration);
   }, 2000);
 });
-
